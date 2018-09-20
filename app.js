12f13b0bba8beb737ad149443a8cd4763c941e74
jameson141 <jamesonmiyashiro@gmail.com>
2018-09-19 14:38:48 -1000

    no u

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const filterRock =data.filter(song=>{
+    
+    return song.genre === "rock"
+})
+console.log('filterRock', filterRock);
---
+++ data.js
@@ -0,0 +1,79 @@
+const data = [
+    {
+        genre: "rock",
+        artist: "Queen",
+        song: "Don't Stop Me Now",
+        image: "https://www.mugbug.co.uk/media/products/500/hmb.queen_logo.coaster.jpg"
+        
+    },
+    {
+        genre: "rock",
+        artist: "Led Zeppelin",
+        song: "Kashmir",
+        image: "https://i.pinimg.com/236x/8d/ef/d3/8defd3a850275923ce06d42ffd4ddccc--led-zeppelin-album-covers-led-zeppelin-albums.jpg"
+    },
+    {
+        genre: "rock",
+        artist: "Pink Floyd",
+        song: "Comfortably Numb",
+        image: "https://upload.wikimedia.org/wikipedia/fi/thumb/d/d1/PinkFloydDarkSideoftheMoon.jpg/260px-PinkFloydDarkSideoftheMoon.jpg"
+    },
+    {
+        genre: "rock",
+        artist: "The Rolling Stones",
+        song: "I Can't Get No Satisfaction",
+        image: "https://i.ebayimg.com/00/s/MTYwMFgxMjAw/z/Br4AAOSw241YjMAm/$_1.JPG?set_id=8800005007"
+    },
+    {
+        genre: "rock",
+        artist: "The Beatles",
+        song: "Ticket To Ride",
+        image: "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/112011/the_beatles.gif?itok=6gZw5fdK"
+    },
+    {
+        genre: "rock",
+        artist: "David Bowie",
+        song: "Space Oddity",
+        image: "https://img.customon.com/design/600/600/ffffff/52742/5200608b10a4ac191978d7286297ffe2.png.jpg"
+    },
+    {
+        genre: "the good stuff",
+        artist: "Robbie Rotten",
+        song: "We Are Number One",
+        image: "https://upload.wikimedia.org/wikipedia/en/5/54/We_Are_Number_One.jpg"
+    },
+    {
+        genre: "the good stuff",
+        artist: "Luis Fonsi",
+        song: "Despacito",
+        image: "https://i.chzbgr.com/original/5809413/h184437FB/"
+    },
+    {
+        genre: "the good stuff",
+        artist: "Toto",
+        song: "Africa",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/Toto_-_Africa.jpg/220px-Toto_-_Africa.jpg"
+    },
+    {
+        genre: "the good stuff",
+        artist: "Smash Mouth",
+        song: "All Star",
+        image: "https://www.dailydot.com/wp-content/uploads/58e/6b/336533f1a5c6b496126044189632f128.jpg" 
+    },
+    {
+        genre: "the good stuff",
+        artist: "Andrew Gold",
+        song: "Spooky Scary Skeletons",
+        image: "https://i.kym-cdn.com/entries/icons/mobile/000/018/709/spookybois.jpg"
+    },
+    {
+        genre: "the good stuff",
+        artist: "Boney M",
+        song: "Rasputin",
+        image: "https://vignette.wikia.nocookie.net/venturiantale/images/4/47/General_Grievous-_Ra_Ra_Rasputin/revision/latest?cb=20170419041648"
+    }
+]
+
+
+    
+
