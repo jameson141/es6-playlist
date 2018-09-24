dbd2aeba8e124232fe26a81e80fc17ca06786a4e
jameson141 <jamesonmiyashiro@gmail.com>
2018-09-24 11:21:31 -1000

    no u

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
-
+function rock () {
 const filterRock =data.filter(song=>{
     
     return song.genre === "rock"
 })
 console.log('filterRock', filterRock);
+map(function (genre){
+    return
+    `
+    <div class = "rockCategory">
+        <img src="${genre.image}" class>="images">
+        <div>Artist: ${genre.artist}</div>
+        <div>Song: ${genre.song}</div>
+        
+    </div>
+    `
+})
+}
+
+function best () {
+const filterBest =data.filter(song=>{
+    return song.genre === "best"
+})  
+console.log('filterBest', filterBest);
+map(function (genre){
+    return
+    `
+    <div class = "bestCategory">
+        <img src="${genre.image}" class>="images">
+        <div>Artist: ${genre.artist}</div>
+        <div>Song: ${genre.song}</div>
+        
+    </div>
+    `
+})
+}
+
+
+
+
+
+
+
+
--- data.js
+++ data.js
@@ -2,14 +2,14 @@ const data = [
     {
         genre: "rock",
         artist: "Queen",
-        song: "Don't Stop Me Now",
+        song: "Stone Cold Crazy",
         image: "https://www.mugbug.co.uk/media/products/500/hmb.queen_logo.coaster.jpg"
         
     },
     {
         genre: "rock",
         artist: "Led Zeppelin",
-        song: "Kashmir",
+        song: "When The Levee Breaks",
         image: "https://i.pinimg.com/236x/8d/ef/d3/8defd3a850275923ce06d42ffd4ddccc--led-zeppelin-album-covers-led-zeppelin-albums.jpg"
     },
     {
@@ -33,41 +33,41 @@ const data = [
     {
         genre: "rock",
         artist: "David Bowie",
-        song: "Space Oddity",
+        song: "Starman",
         image: "https://img.customon.com/design/600/600/ffffff/52742/5200608b10a4ac191978d7286297ffe2.png.jpg"
     },
     {
-        genre: "the good stuff",
+        genre: "best",
         artist: "Robbie Rotten",
         song: "We Are Number One",
         image: "https://upload.wikimedia.org/wikipedia/en/5/54/We_Are_Number_One.jpg"
     },
     {
-        genre: "the good stuff",
+        genre: "best",
         artist: "Luis Fonsi",
         song: "Despacito",
         image: "https://i.chzbgr.com/original/5809413/h184437FB/"
     },
     {
-        genre: "the good stuff",
+        genre: "best",
         artist: "Toto",
         song: "Africa",
         image: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/Toto_-_Africa.jpg/220px-Toto_-_Africa.jpg"
     },
     {
-        genre: "the good stuff",
+        genre: "best",
         artist: "Smash Mouth",
         song: "All Star",
         image: "https://www.dailydot.com/wp-content/uploads/58e/6b/336533f1a5c6b496126044189632f128.jpg" 
     },
     {
-        genre: "the good stuff",
+        genre: "best",
         artist: "Andrew Gold",
         song: "Spooky Scary Skeletons",
         image: "https://i.kym-cdn.com/entries/icons/mobile/000/018/709/spookybois.jpg"
     },
     {
-        genre: "the good stuff",
+        genre: "best",
         artist: "Boney M",
         song: "Rasputin",
         image: "https://vignette.wikia.nocookie.net/venturiantale/images/4/47/General_Grievous-_Ra_Ra_Rasputin/revision/latest?cb=20170419041648"
