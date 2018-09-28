fe97778bcb3fd7a30a53e1c10bd97c8724d476ba
jameson141 <30993110+jameson141@users.noreply.github.com>
2018-09-28 12:13:41 -1000

    Still in Pro-gress

--- app.js
+++ app.js
@@ -1,51 +1,16 @@
-function rockSection () {
-const filterRock =data.filter(song=>{
-    
-    return song.genre === "rock"
-})
+const containerMusic = document.querySelector('#musicContainer');
+var infoArray = [];
 
-.map((songs) => {
-    console.log('rock', song);
-    return
-    `
-    <div class = "rockCategory">
-        <img src="${genre.image}" class>="images">
-        <div>Artist: ${genre.artist}</div>
-        <div>Song: ${genre.song}</div>
-        
-    </div>
-    `
-})
-
-
-   
+for(var i=0; i < data.length; i++) {
+    let temp = document.createElement('div');
+    temp.innerHTML = `
+        <img src=${data[i].image}>
+        <h2> ${data[i].song} </h2>
+        <h3> ${data[i].artist} <h3>
+        <h4> ${data[i].genre} </h3>
+    `;
+    temp.classList.add('Containers');
+    infoArray.push(temp);
+    containerMusic.appendChild(temp);
 }
-
-function bestSection () {
-const filterBest =data.filter(song=>{
-    return song.genre === "best"
-})  
-
-.map((songs) => {
-    console.log('best', musical);
-    return
-    `
-    <div class = "bestCategory">
-        <img src="${genre.image}" class>="images">
-        <div>Artist: ${genre.artist}</div>
-        <div>Song: ${genre.song}</div>
-        
-    </div>
-    `
-})
-   
-
-}
-
-
-
-
-
-
-
 
